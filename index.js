fa9b056254fbcecce16e56f8684e8de92d3e0388
arunsahoo-xt <armodilo60@gmail.com>
2023-03-18 00:27:35 +0530

    checking image loading

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-var outImage ="original-image";
-var canvas = document.getElementById('canvas'),
-        ctx = canvas.getContext('2d');
+var outImage =".original-image";
+// var canvas = document.getElementById('canvas'),
+//         ctx = canvas.getContext('2d');
 // canvas.width = $('#imagenFondo').width();
 
 
-canvas.crossOrigin = "Anonymous";
-canvas.width = 300;
-canvas.height =300;
-var image_ele=document.getElementById(outImage);
+// canvas.crossOrigin = "Anonymous";
+// canvas.width = 300;
+// canvas.height =300;
+var image_ele=document.querySelector(outImage);
 var image_container=document.querySelector(".image-container");
-console.log(image_container);
-ctx.drawImage(image_ele, 0, 0,image_ele.width,image_ele.height);
-ctx.font = "36pt Impact";
+// console.log(image_container);
+// ctx.drawImage(image_ele, 0, 0,image_ele.width,image_ele.height);
+// ctx.font = "36pt Impact";
+
 
 
 /********* Handling checkbox and toggle textbox *********/
@@ -36,10 +37,11 @@ function hide_textbox(textbox_name,checkbox_name){
 }
 /* **************************************************** */
 
+
 $(document).on('input','#inp-top',function(){
     // //redraw image
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.drawImage(image_ele, 0, 0,image_ele.width,image_ele.height);
+    // ctx.clearRect(0,0,canvas.width,canvas.height);
+    // ctx.drawImage(image_ele, 0, 0,image_ele.width,image_ele.height);
     // //refill text
     // ctx.fillStyle = "black";
     // ctx.fillText($(this).val(),50,50);
@@ -48,28 +50,71 @@ $(document).on('input','#inp-top',function(){
 });
 $(document).on('input','#inp-bot',function(){
     // //redraw image
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.drawImage(image_ele, 0, 0,image_ele.width,image_ele.height);
-    // //refill text
-    // ctx.fillStyle = "white";
-    // ctx.fillText($(this).val(),50,350);
-
+    // ctx.clearRect(0,0,canvas.width,canvas.height);
+    // ctx.drawImage(image_ele, 0, 0,image_ele.width,image_ele.height);
 	var bot_inp=document.querySelector(".drag-bot");
 	bot_inp.innerHTML=$(this).val();
 });
 
 
 
-var formObj=document.querySelector('.formSelector');
-console.log(formObj);
-$('button').click(function(){
-    // console.log(ctx.getImageData(50, 50, 100, 100));
-    event.preventDefault();
+
+//**************code to download meme********************
+
+$('button').click(function(event){
+	// console.log(ctx.getImageData(50, 50, 100, 100));
+	event.preventDefault();
 	console.log(document.getElementById(outImage));
+	printToFile();
 });
 
+
+function printToFile() {
+	var wrapper = document.getElementById('#wrapperid');
+
+
+		html2canvas(wrapper).then(canvas => {
+			canvas.crossOrigin = "Anonymous";
+			var myImage = canvas.toDataURL("image/png");//it actually returns a canvas then converted to DataURI
+            //Then download file
+            downloadURI("data:" + myImage, "yourMeme.png");
+		  });
+		  
+	  
+}
+//Creating dynamic link that automatically click
+function downloadURI(uri, name) {
+	var link = document.createElement("a");
+	link.download = name;
+	link.href = uri;
+	link.click();
+	//after creating link you should delete dynamic link
+	//clearDynamicLink(link); 
+}
+// *******************************************************
+
+
+// window.addEventListener("load", (event) => {
+// 	var copy_image=document.querySelector(".copy-image");
+// 	var image=new Image();
+// 	image.src="yourMeme (9).png";
+// 	copy_image.src=image.src;
+//   });
+
+function preview_1(obj){
+	console.log(obj);
+	var copy_image=document.querySelector(".copy-image");
+	var image=new Image();
+	image.crossOrigin = "Anonymous";
+	image.src=obj.src;
+		image.onload = function () {
+			image_ele.src=image.src;
+			copy_image.src=image.src;
+		};
+}
 function preview_2(obj)
 {
+	var copy_image=document.querySelector(".copy-image");
 	if (FileReader)
 	{
 		var reader = new FileReader();
@@ -79,6 +124,7 @@ function preview_2(obj)
 		image.src=e.target.result;
 		image.onload = function () {
 			image_ele.src=image.src;
+			copy_image.src=image.src;
 		};
 		}
 	}
